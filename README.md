e0590689500016600105e63d37abb9c18069253c
Josh Rickard <josh.rickard@phishme.com>
2018-07-13 21:56:08 -0400

    Adding LocalChocolateyPackage to repo

---
+++ README.md
@@ -0,0 +1,57 @@
+# LocalChocolateyPackage
+
+[![Build status](https://ci.appveyor.com/api/projects/status/1q01o9rv2u1cu967?svg=true)](https://ci.appveyor.com/project/MSAdministrator/localchocolateypackage)
+
+## Creates and installs local Chocolatey packages
+
+LocalChocolateyPackage can create and install local Chocolatey packages
+
+## Synopsis
+
+A PowerShell Module to create and install local Chocolatey packages.
+
+## Description
+
+A PowerShell Module to create and install local Chocolatey packages.
+
+## Using LocalChocolateyPackage
+
+To use this module, you will first need to download/clone the repository and import the module:
+
+```powershell
+Import-Module .\LocalChocolateyPackage.psm1
+```
+
+### Generating Local Chocolatey Packages
+
+Once you have imported the module you can generate new Chocolatey pacakges.  You must specify the the `SourcePath` containing your nuspec folder structure and the `OutputPath` to store the generated nupkg packages.
+
+Before you can generate packages you must have follow the Chocolatey guidelines for folder layout.  Here is an example:
+
+```text
+You must have a Chocolatey specified folder structure for nuspec generation:
+    C:\Packages\PackageName\
+        C:\Packages\PackageName.nuspec
+        C:\Packages\tools\chocolateyInstall.ps1
+    C:\NewPackages\
+```
+
+```powershell
+New-LocalChocolateyPackage -SourcePath 'C:\Packages' -OutputPath 'C:\NewPackages'
+```
+
+### Installing Local NuGet Packages
+
+To install your own nupkg you can run the `Install-LocalChocolateyPackage` function:
+
+```powershell
+Install-LocalChocolateyPackage -PackageName 'SomePackageName' -PackageSource 'C:\some\path_to_folder_containing_packages'
+```
+
+## Notes
+
+```yaml
+   Name: LocalChocolateyPackage
+   Created by: Josh Rickard (MSAdministrator)
+   Created Date: 07/13/2018
+```
---
+++ Tests/LocalChocolateyPackage.Tests.ps1
@@ -0,0 +1,73 @@
+$here = "$(Split-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) -Parent)\LocalChocolateyPackage"
+
+$module = 'LocalChocolateyPackage'
+
+Describe "$module PowerShell Module Tests" {
+    Context 'Module Setup' {
+        It "has the root module $module.psm1" {
+            "$here\$module.psm1" | Should -Exist
+        }
+        It "has the manifest file $module.psd1" {
+            "$here\$module.psd1" | should exist
+        }
+        It "$module has functions" {
+            "$here\Public\*.ps1" | Should exist
+            "$here\Private\*.ps1" | should exist
+        }
+        It "$module is valid PowerShell Code" {
+            $psFile = Get-Content -Path "$here\$module.psm1" -ErrorAction Stop
+            $errors = $null
+            $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+            $errors.count | Should be 0
+        }
+    }
+}
+
+$pubFunctions = ('New-LocalChocolateyPackage','Install-LocalChocolateyPackage')
+
+$folders = ( 'Public')
+
+foreach ($folder in $folders) {
+    Describe 'Folders Tests' {    
+        It "$folder should exist" {
+            "$here\$Folder" | Should Exist
+        }
+    }
+}
+
+Describe 'Function Tests' {
+    foreach ($function in $pubFunctions) {
+        Context 'Public Functions' {
+            It "$function.ps1 should exist" {
+                "$here\Public\$function.ps1" | Should Exist
+            }
+            It "$function.ps1 should have help block" {
+                "$here\Public\$function.ps1" | Should -FileContentMatch '<#'
+                "$here\Public\$function.ps1" | Should -FileContentMatch '#>'
+            }
+            It "$function.ps1 should have a SYNOPSIS section in the help block" {
+                "$here\Public\$function.ps1" | Should -FileContentMatch '.SYNOPSIS'
+            }
+            It "$function.ps1 should have a DESCRIPTION section in the help block" {
+                "$here\Public\$function.ps1" | Should -FileContentMatch '.DESCRIPTION'
+            }
+            It "$function.ps1 should have a EXAMPLE section in the help block" {
+                "$here\Public\$function.ps1" | Should -FileContentMatch '.EXAMPLE'
+            }
+            It "$function.ps1 should be an advanced function" {
+                "$here\Public\$function.ps1" | Should -FileContentMatch 'function'
+                "$here\Public\$function.ps1" | Should -FileContentMatch 'CmdLetBinding'
+                "$here\Public\$function.ps1" | Should -FileContentMatch 'param'
+            }
+            It "$function.ps1 should contain Write-Verbose blocks" {
+                "$here\Public\$function.ps1" | Should -FileContentMatch 'Write-Verbose'
+            }
+            It "$function.ps1 is valid PowerShell code" {
+                $psFile = Get-Content -Path "$here\Public\$function.ps1" -ErrorAction Stop
+                $errors = $null
+                $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+                $errors.count | Should be 0
+            } 
+        }# Context Public Function Tests
+    } # foreach for public functions
+} # end of describe block
